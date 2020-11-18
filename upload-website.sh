734559eeb24a6e723762c20702518f6dfabee768
davewripley <davewripley@gmail.com>
2020-11-18 12:10:36 +1100

    Tweak upload script for new host

--- upload-website.sh
+++ upload-website.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -avz /Users/user_old/Dropbox/Haskell/website/for-upload/ davewripley@davewripley.rocks:/big/dom/xdavewripley/www/
+rsync -avz /Users/user_old/Dropbox/Haskell/website/for-upload/ davewrip@davewripley.rocks:~/public_html/
