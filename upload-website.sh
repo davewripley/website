f31fe72d74611cc0ab3c6c49512a0e26246d9bc6
David Ripley <davewripley@gmail.com>
2021-10-29 13:34:59 +1100

    Fix upload script for new username

--- upload-website.sh
+++ upload-website.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -avz /Users/user_old/Dropbox/programming/Haskell/website/for-upload/ davewrip@davewripley.rocks:~/public_html/
+rsync -avz /Users/dave/Dropbox/programming/Haskell/website/for-upload/ davewrip@davewripley.rocks:~/public_html/
